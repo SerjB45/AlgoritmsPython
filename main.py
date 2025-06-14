172f70c7f2aefc5c6038ae68d50b0a031fecc92c
Serj Bakumec <sbakumtsev@yandex.ru>
2025-06-14 16:27:34 +0300

    quick merge

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dijkstra import *
 from bellman_ford import *
 from greedy import *
 from knn_algorithm import *
+from quick_merge import *
 
 print('Run')
 lst1 = [9, 65, 4, 4, 6, 0, 5, 2, 1]
@@ -32,3 +33,7 @@ test_greedy_knapsack()
 test_interval_scheduling()
 test_nearest_neighbor_tsp()
 test_knn_classify()
+
+list1 = [3, 10, 11, 12, 47, 57, 58, 63, 77, 79, 80, 95]
+list2 = [0, 11, 12, 20, 24, 26, 47, 48, 53, 65, 70, 81, 84, 84, 90]
+print( quick_merge(list1, list2))
